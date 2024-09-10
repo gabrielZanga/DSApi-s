0c4a49e28faabb2fbc999f79042b0faa56ae9e02
gah__vi <138242215+gabrielZanga@users.noreply.github.com>
2024-09-10 12:26:05 -0300

    Add files via upload

---
+++ estoque3/src/main/java/com/br/example/estoque/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.br.example.estoque.repositories;
+
+import com.br.example.estoque.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
