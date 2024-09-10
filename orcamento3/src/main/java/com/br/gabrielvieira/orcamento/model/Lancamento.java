f8f893ca3e23713067fbb5223c80aab120e49426
gah__vi <138242215+gabrielZanga@users.noreply.github.com>
2024-09-10 12:25:26 -0300

    Add files via upload

---
+++ orcamento3/src/main/java/com/br/gabrielvieira/orcamento/model/Lancamento.java
@@ -0,0 +1,82 @@
+package com.br.gabrielvieira.orcamento.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name="lancamento")
+public class Lancamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate datalancamento;
+
+    private String tipolancamento;
+    private BigDecimal valorlancamento;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+    //GETTERS AND SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDatalancamento() {
+        return datalancamento;
+    }
+
+    public void setDatalancamento(LocalDate datalancamento) {
+        this.datalancamento = datalancamento;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getTipolancamento() {
+        return tipolancamento;
+    }
+
+    public void setTipolancamento(String tipolancamento) {
+        this.tipolancamento = tipolancamento;
+    }
+
+    public BigDecimal getValorlancamento() {
+        return valorlancamento;
+    }
+
+    public void setValorlancamento(BigDecimal valorlancamento) {
+        this.valorlancamento = valorlancamento;
+    }
+
+    //EQUALS AND HASH CODE
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lancamento that = (Lancamento) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
