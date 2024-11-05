449b053bdfbee5102f3fd9ab21876f9f29a482dd
gah__vi <138242215+gabrielZanga@users.noreply.github.com>
2024-11-05 12:20:37 -0300

    Delete estoque3 directory

--- estoque3/src/main/java/com/br/example/estoque/repositories/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.example.estoque.repositories;
-
-import com.br.example.estoque.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
