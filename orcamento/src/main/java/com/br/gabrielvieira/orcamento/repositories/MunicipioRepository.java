05c97852931ce1a8603372ba0b3c429a1a5f5d06
gah__vi <138242215+gabrielZanga@users.noreply.github.com>
2024-11-05 12:19:21 -0300

    Delete orcamento directory

--- orcamento/src/main/java/com/br/gabrielvieira/orcamento/repositories/MunicipioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.gabrielvieira.orcamento.repositories;
-
-import com.br.gabrielvieira.orcamento.model.Municipio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MunicipioRepository extends JpaRepository<Municipio, Integer> {
-}
